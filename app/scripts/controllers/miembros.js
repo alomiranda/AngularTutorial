8d89f932da14bcfa4248717f7ad1258d506de24c
alomiranda <alomtv2@gmail.com>
2015-07-05 14:11:41 -0600

    miembros

--- app/scripts/controllers/miembros.js
+++ app/scripts/controllers/miembros.js
@@ -8,13 +8,63 @@
  * Controller of the prototiposApp
  */
 angular.module('prototiposApp')
-  .controller('MiembrosCtrl', function ($scope, $http) {
+  .controller('MiembrosCtrl', function ($scope, $http, $modal) {
     
     $http.get('http://localhost:9000/miembros.json').success(function(data){
     	$scope.miembros = data;
-    })
+    });
 
     $scope.gridOptions = {
-    	data: 'miembros'
+    	data: 'miembros',
+        showGroupPanel: true,
+        enableCellSelection: true,
+        enableRowSelection: false,
+        enableCellEdit: true,
+    	columnDefs: [
+    		{field: 'no', displayName: 'N°.'},
+    		{field: 'nombre', displayName: 'Nombre'},
+    		{field: 'fidelidad', displayName: 'Puntos Fidelidad'},
+    		{field: 'fechaUnion', displayName: 'Fecha de Union'},
+    		{field: 'tipoMiembro', displayName: 'Tipo de Miembro'}
+    	]
     };
+
+    $scope.showModal=function(){
+        $scope.nuevoMiembro={};
+        var modalInstance = $modal.open({
+            templateUrl: 'views/add-miembros.html',
+            controller: 'AddNuevoMiembroCtr',
+                resolve: {
+                    nuevoMiembro: function() {
+                        return $scope.nuevoMiembro;
+                    }
+                }
+        });
+
+        modalInstance.result.then(function (selectItem){
+            $scope.miembros.push({
+                no: $scope.miembros.length + 2,
+                nombre: $scope.nuevoMiembro.nombre,
+                fedelidad: $scope.nuevoMiembro.fidelidad,
+                fechaUnion: $scope.nuevoMiembro.fechaUnion,
+                tipoMiembro: $scope.nuevoMiembro.tipoMiembro
+            });
+            console.log($scope.miembros);
+        });
+        
+    };
+
+
+  })
+
+  .controller('AddNuevoMiembroCtr', function($scope,$modalInstance, nuevoMiembro){
+        $scope.nuevoMiembro = nuevoMiembro;
+        $scope.salvarNuevoMiembro = function(){
+           $modalInstance.close(nuevoMiembro);
+          console.log('holamundo');
+        };
+
+        $scope.cancel = function(){
+            $modalInstance.dismiss('cancel');
+        };
   });
